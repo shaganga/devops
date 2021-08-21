23207128029026c9f1f1c06253f1f847c02aaff0
shaganga <iamshankar.g@gmail.com>
2021-08-21 18:19:33 +0530

    Update jenkinsfilenexus.groovy

--- cicd/jenkinsfilenexus.groovy
+++ cicd/jenkinsfilenexus.groovy
@@ -50,9 +50,12 @@ stage ('Cloning Git') {
                 script {
 			dir ("$WORKSPACE") {
 			     sh "echo ${nexus_user}"
-				sh "curl -LO -k -u ${nexus_user}:${nexus_pwd} -X GET ${nexus_url}/repository/mvn-repo/dev/ribhus/0.1.0/ribhus-0.1.0.jar"
+				withCredentials([usernamePassword(credentialsId: 'nexus-access', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+ 
+
+				sh "curl -LO -k -u ${USERNAME}:${PASSWORD} -X GET ${nexus_url}/repository/mvn-repo/dev/ribhus/0.1.0/ribhus-0.1.0.jar"
 			     
-			}
+			}}
 		}}}
 
 }
