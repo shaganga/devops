5abc4047634c00505b7ee330709850576b791bd6
shaganga <iamshankar.g@gmail.com>
2021-08-21 15:58:19 +0530

    Update jenkinsfilenexus.groovy

--- cicd/jenkinsfilenexus.groovy
+++ cicd/jenkinsfilenexus.groovy
@@ -1,1 +1,25 @@
+pipeline {
 
+environment {
+repo_name = "cicd_repo"
+
+dockerImage = ''
+}
+agent any
+stages {
+stage ('Cloning Git') {
+  steps {
+  git 'https://github.com/dstar55/docker-hello-world-spring-boot.git'
+        }
+    }
+
+  stage('Stage: Maven Build'){    
+            steps {
+                //Generate Artifact through maven build       
+            sh "mvn clean install"             
+        }
+    }
+  
+}
+}
+}
