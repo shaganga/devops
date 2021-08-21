5665204f4d984b2fce317116525c99f0d27dfce1
shaganga <iamshankar.g@gmail.com>
2021-08-21 17:57:37 +0530

    Update jenkinsfilenexus.groovy

--- cicd/jenkinsfilenexus.groovy
+++ cicd/jenkinsfilenexus.groovy
@@ -50,7 +50,7 @@ stage ('Cloning Git') {
                 script {
 			dir (/tmp) {
 			
-	sh 'curl -LO -u admin:C@c0nuts -X GET "http://54.145.82.82:8081/repository/mvn-repo/dev/ribhus/0.1.0/ribhus-0.1.0.jar"'
+			     sh 'curl -LO -u ${nexus_user}:${nexus_pwd} -X GET http://54.145.82.82:8081/repository/mvn-repo/dev/ribhus/0.1.0/ribhus-0.1.0.jar'
 			}
 		}}}
 
